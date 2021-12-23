68dfdd19f4af45055921348f04d85a7217617d4d
Candi Chiu <li-lun.chiu@kfzteile24.de>
2021-12-23 16:21:28 +0100

    FlashChat-CompletediOS13

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Messages] = [
+      Messages(sender: "1@2.de", body: "Hi"),
+      Messages(sender: "2@3.de", body: "Hey"),
+      Messages(sender: "1@2.de", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        title = K.appName
+        navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
+}
 
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+        
+    }
+    
+    
 }
---
+++ Flash Chat iOS13/Messages.swift
@@ -0,0 +1,12 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Candi Chiu on 19.12.21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+struct Messages {
+    let sender : String
+    let body : String
+}
